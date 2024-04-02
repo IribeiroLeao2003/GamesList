8defcae58d9c0a831feae8de32be31c4fb15441e
IribeiroLeao2003 <iribeiroleao9600@conestogac.on.ca>
2024-04-02 13:15:30 -0400

    Adding navbar to project

--- src/posts/views.py
+++ src/posts/views.py
@@ -47,7 +47,7 @@ def post_detail(request, pk):
 
     return render(request, 'posts/detail.html', context)
 
-
+@login_required
 def load_post_data_view(request, num_posts):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         visible = 3 
@@ -68,7 +68,9 @@ def load_post_data_view(request, num_posts):
             }
             data.append(item)
         return JsonResponse({'data': data[lower:upper], 'size': size})
+    
 
+@login_required
 def post_detail_data_view(request, pk):
     obj = Post.objects.get(pk=pk)
     data = {
@@ -80,6 +82,8 @@ def post_detail_data_view(request, pk):
     }
     return JsonResponse({'data': data})
 
+
+@login_required
 def like_unlike_post(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         pk = request.POST.get('pk')
@@ -91,7 +95,10 @@ def like_unlike_post(request):
             liked = True
             obj.liked.add(request.user)
         return JsonResponse({'liked': liked, 'count': obj.like_count})
+    
 
+@login_required
+@action_permission
 def update_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -114,6 +121,8 @@ def image_upload_view(request):
         Photo.objects.create(image=img, post=post)
     return HttpResponse()
 
+
+@login_required
 @action_permission
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
