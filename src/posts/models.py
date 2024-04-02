c17f4d16f85f6df6f531f804299643b2288d926b
IribeiroLeao2003 <iribeiroleao9600@conestogac.on.ca>
2024-04-01 21:05:07 -0400

    Finished doing posts, moving to a description box as said in the tutorials

--- src/posts/models.py
+++ src/posts/models.py
@@ -19,5 +19,8 @@ class Post(models.Model):
     @property
     def like_count(self):
         return self.liked.all().count()
+    
+    class Meta:
+        ordering = ("-created",)
 
 
--- src/posts/views.py
+++ src/posts/views.py
@@ -16,6 +16,13 @@ def post_list_and_create(request):
             instance = form.save(commit=False)
             instance.author = author 
             instance.save()
+            return JsonResponse({
+            'title': instance.title,
+            'body': instance.body,
+            'author': instance.author.user.username,
+            'id': instance.id
+        })
+
 
     context = {
     'form': form,
@@ -24,25 +31,39 @@ def post_list_and_create(request):
 
     return render(request, 'posts/main.html', context)
 
-def load_post_data_view(request, num_posts):
-    visible = 3 
-    upper = num_posts
-    lower = upper - visible
-    size = Post.objects.all().count() 
 
-    qs = Post.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id': obj.id,
-            'title': obj.title,
-            'body': obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author': obj.author.user.username
-        }
-        data.append(item)
-    return JsonResponse({'data': data[lower:upper], 'size': size})
+def post_detail(request, pk):
+    obj = Post.objects.get(pk=pk)
+    form = PostForm()
+
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+
+    return render(request, 'posts/detail.html', context)
+
+
+def load_post_data_view(request, num_posts):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        visible = 3 
+        upper = num_posts
+        lower = upper - visible
+        size = Post.objects.all().count() 
+
+        qs = Post.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author': obj.author.user.username
+            }
+            data.append(item)
+        return JsonResponse({'data': data[lower:upper], 'size': size})
 
 
 def like_unlike_post(request):
